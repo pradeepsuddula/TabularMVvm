87a51ad3f96b22ad69816d522d9c3005aae53b7e
pradeepsuddula <102672004+pradeepsuddula@users.noreply.github.com>
2023-10-12 17:01:25 +0530

    Add files via upload

---
+++ TabularMvvm (1)/TabularMvvm/TabularMvvm/Model/EmployeeData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabularMvvm.Model
+{
+    public class EmployeeData:INotifyPropertyChanged
+    {
+        private long id;
+        private string name;
+        private string role;
+
+        public long Id { 
+            get => id   ;
+            set
+            {
+                id = value;
+                OnPropertyChange(nameof(Id));
+            } 
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChange(nameof(name));
+            }
+        }
+        public string Role
+        {
+            get => role;
+            set
+            {
+                role = value;
+                OnPropertyChange(nameof(role));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
